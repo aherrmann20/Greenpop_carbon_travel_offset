dc6be9d6e5bbbfea7b8e2b8155d586a403f8053a
aherrmann20 <aherrmann@college.harvard.edu>
2015-07-14 13:40:48 +0200

    Created working link to download CSV

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :volunteers
-  resources :transportations
+    resources :transportations
 
   root "volunteers#new"
   # The priority is based upon order of creation: first created -> highest priority.
