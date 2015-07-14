1ac3b845f8832a3479974d7bc85bae4a320cde40
aherrmann20 <aherrmann@college.harvard.edu>
2015-07-14 10:29:34 +0200

    Calculating and displaying co_weight and tree_offset

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Transportation.delete_all
+Volunteer.delete_all
 
 puts "Creating volunteers..."
 puts "Creating Transportation..."
+
 
 Transportation.create!(mode: "Car")
 Transportation.create!(mode: "SUV")
